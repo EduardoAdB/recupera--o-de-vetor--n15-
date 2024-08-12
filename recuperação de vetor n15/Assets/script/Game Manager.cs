9017b030f78e678877192b451a5be14a5e760f40
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-08-12 09:19:18 -0300

    variaveis criadas

---
+++ recuperação de vetor n15/Assets/script/Game Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int corDaVez;
+    int acertos;
+    int erros;
+
+    int[] sequencia;
+
+    [SerializeField]
+    string[] nomes;
+
+
+    
+}
