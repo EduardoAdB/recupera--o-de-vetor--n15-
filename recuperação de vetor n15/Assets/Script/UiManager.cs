d765f14192616bf385b28441fb79972fa77dd0a9
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-08-12 10:16:53 -0300

    Atualizar acertos e erros feito

--- recuperação de vetor n15/Assets/Script/UiManager.cs
+++ recuperação de vetor n15/Assets/Script/UiManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region.singleton
+    Button[] botoes;
+    [SerializeField]
+    TextMeshProUGUI sequenciaTexto;
+    [SerializeField]
+    TextMeshProUGUI acertouTexto;
+    [SerializeField]
+    TextMeshProUGUI errouTexto;
+    #endregion
+    private void Start()
     {
-        
+        for (int i = 0; i < botoes.Length; i++)
+        {
+            int x = i;
+            //botoes[i].onClick(GameManager.ChecarCor(x));
+        }
+    }
+    public void AtualizarAcertos(int acertos)
+    {
+        acertouTexto.text = acertos.ToString();
+    }
+    public void AtualizarErros(int erros)
+    {
+        errouTexto.text = erros.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
