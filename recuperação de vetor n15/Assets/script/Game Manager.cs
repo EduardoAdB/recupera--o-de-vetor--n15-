8016c25df7f335470840211e191b22c37b2e4bac
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-08-12 09:19:51 -0300

    metodos start e gerar sequencias criados

--- recuperação de vetor n15/Assets/script/Game Manager.cs
+++ recuperação de vetor n15/Assets/script/Game Manager.cs
@@ -15,5 +15,22 @@ public class GameManager : MonoBehaviour
     string[] nomes;
 
 
+    private void Start()
+    {
+        GerarSequencia();
+    }
+
+    public void GerarSequencia()
+    {
+        corDaVez = 0;
+        sequencia = new int[Random.Range(3, nomes.Length)];
+        //UiManager.instance.LimparTexto();
+        for (int i = 0; i < sequencia.Length; i++)
+        {
+            sequencia[i] = Random.Range(3, nomes.Length);
+            //UiManager.instance.AtualizarSequencia(nomes[i]);
+            //Att pra validar os comit
+        }
+    }
     
 }
